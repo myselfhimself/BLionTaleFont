b50a54e15bfb08783f3abdd14319ece152af69a3
Jonathan-David Schröder <jonathan.schroder@gmail.com>
2020-02-14 14:24:54 +0100

    proper files naming, script shows proper image width from characters length

--- preview.sh
+++ preview.sh
@@ -2,4 +2,8 @@
 # Usage: <this script> "text to preview"
 # Requires: image magick
 FONT_TEMPFILE=hop.png
-convert -size 165x70 -pointsize 120 -font MyFont-Regular.otf label:"$1" $FONT_TEMPFILE; display $FONT_TEMPFILE; rm $FONT_TEMPFILE
+FONT_FILE=BLionTale-Regular.otf
+PREVIEW_LABEL=${1:-ABCDabcd}
+CHARS_LENGTH=$(echo $PREVIEW_LABEL | tr -d "\n" | wc -c | tr -d " ")
+PREVIEW_WIDTH=$(($CHARS_LENGTH * 60))
+convert -size "$PREVIEW_WIDTH"x70 -pointsize 120 -font "$FONT_FILE" label:"$PREVIEW_LABEL" $FONT_TEMPFILE; display $FONT_TEMPFILE; rm $FONT_TEMPFILE
