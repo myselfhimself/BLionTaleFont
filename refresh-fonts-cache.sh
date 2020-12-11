9dacb0ed8a2f8ca6948c6957a76d25572c8a24b3
Jonathan-David Schröder <jonathan.schroder@gmail.com>
2020-12-11 14:02:44 +0100

    adding tree

--- refresh-fonts-cache.sh
+++ refresh-fonts-cache.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 sudo mkdir -p /usr/share/fonts/opentype/bliontale/
-sudo cp BLionTale-Regular.otf /usr/share/fonts/opentype/bliontale/
+sudo cp -f BLionTale-Regular.otf /usr/share/fonts/opentype/bliontale/
 sudo fc-cache -f -v
